e94318474ff6e5e96b474d1b8eb15adf4f6bee50
cheesecat47 <cheesecat47@gmail.com>
2023-01-25 00:18:30 +0900

    [Add] Add Footer

--- frontend/src/shared/App.js
+++ frontend/src/shared/App.js
@@ -3,6 +3,7 @@ import styles from './App.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Home, About } from '../pages';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MyFooter from "../components/Footer";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <div className={styles.App}>
         <RouterProvider router={router} />
+        <MyFooter />
       </div>
     );
   }
