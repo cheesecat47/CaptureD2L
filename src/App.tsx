c25bbcd29c53c3d1445f51de93b914f1394b98a7
cheesecat47 <cheesecat47@gmail.com>
2023-06-09 15:11:32 +0900

    Replace sample image

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Header } from "./components/header.tsx";
 import { Footer } from "./components/footer.tsx";
+import sampleBefore from "./assets/sample_before.png";
+import sampleAfter from "./assets/sample_after.png";
 
 // ----------------------------------------------------------------
 // import jimp: https://github.com/jimp-dev/jimp/issues/1091#issuecomment-1420811585
@@ -156,40 +158,8 @@ function App() {
             id="resultBox"
             className="my-10 columns-1 items-center justify-center gap-2 text-center sm:columns-2"
           >
-            {imgBefore ? (
-              <img src={imgBefore} alt="Before" className="w-full" />
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="mx-auto w-48"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            )}
-            {imgAfter ? (
-              <img src={imgAfter} alt="After" className="w-full" />
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="mx-auto w-48"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                />
-              </svg>
-            )}
+            <img src={imgBefore || sampleBefore} alt="Before" className="w-full" />
+            <img src={imgAfter || sampleAfter} alt="After" className="w-full" />
           </div>
         </div>
         <Footer />
